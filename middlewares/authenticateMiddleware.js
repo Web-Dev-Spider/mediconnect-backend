6db549e75c5f21c2c559d37be5c81dd82798bbab
Syam <nmsyam.mohan@gmail.com>
2025-04-01 13:57:43 +0530

    RBAC implemented for routes

--- middlewares/authenticateMiddleware.js
+++ middlewares/authenticateMiddleware.js
@@ -1,13 +1,35 @@
-const authenticate = (req, res, next) => {
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config/envConfig");
+
+const authenticate = async (req, res, next) => {
   try {
     console.log("Passed through auth middleware");
+    const token = req.cookies?.token;
+    if (!token) {
+      return res.status(401).json({ message: "No token found.. Access denied." });
+    }
 
+    const decoded = jwt.verify(token, JWT_SECRET);
+    // console.log("\nrequest received in authenticate\n", req);
+
+    // console.log("\n decoded data: ", decoded);
+    req.user = decoded; // attached the token payload/to the request.
+
+    console.log("\n req.user", req.user);
     next();
   } catch (error) {
-    console.log(error);
-    console.log(typeof error);
-    next(error);
+    console.log("Authentication Middlware error", error);
+    // next(error);
+    res.status(401).json({ message: "Invalid or expired token" });
   }
 };
 
-module.exports = authenticate;
+const authorize = (roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ success: "false", message: "Access denied, Insufficient Permission." });
+    }
+    next();
+  };
+};
+module.exports = { authenticate, authorize };
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,22 @@
 const userRouter = require("express").Router();
 
 // const authenticate = require("../middlewares/authenticateMiddleware");
-const authenticate = require("../middlewares/authenticateMiddleware");
+const { authenticate, authorize } = require("../middlewares/authenticateMiddleware");
 
 const { profile } = require("../controllers/userController");
+const authRouter = require("./authRouter");
 
-userRouter.post("/profile", authenticate, profile);
+//Only admins can access this
+// userRouter.post("/profile", authenticate, profile);
+
+userRouter.get("/admin", authenticate, authorize(["admin"]), (req, res, next) => {
+  res.send("Welcome admin");
+});
+userRouter.get("/doctor", authenticate, authorize(["doctor"]), (req, res, next) => {
+  res.send("Welcome doctor");
+});
+userRouter.get("/patient", authenticate, authorize(["patient"]), (req, res, next) => {
+  res.send("Welcome patient");
+});
 
 module.exports = userRouter;
