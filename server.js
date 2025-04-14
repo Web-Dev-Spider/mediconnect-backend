e788acddb045ab703c52b512dae2add1b444fb27
Syam <nmsyam.mohan@gmail.com>
2025-04-14 23:41:17 +0530

    cross origin url corrected to front end url

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 // app.options("*", cors()); // handles preflight requests
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://mediconnect-zeta.vercel.app/",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
