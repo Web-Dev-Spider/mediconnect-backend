70d16866658b209febb00e4bbeb6d5dff8e8aba8
Syam <nmsyam.mohan@gmail.com>
2025-04-15 19:45:49 +0530

    bcrypt changed to bcryptjs

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 const restrictedUserNames = ["system", "root", "superuser", "test"];
 const validators = require("../utilities/validators");
